90f879e79a0ffa7ed128a71e9a78160872b70469
Mahfuzur Rahman <mdmahfuzur980@gmail.com>
2022-08-16 21:39:39 +0600

    install some package also work navigation

--- vaccine_management_system/src/App.js
+++ vaccine_management_system/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
-import MainComponent from './components/MainComponent';
+import Main from './components/Main';
+import { BrowserRouter }  from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {store} from './redux/store';
 
 
 function App() {
   return (
-    <div className="App">
-        <MainComponent/>
+    <div >
+      <Provider store={store}>
+         <BrowserRouter>
+            <Main/>
+         </BrowserRouter>
+      </Provider>
+     
     </div>
   );
 }
--- vaccine_management_system/src/components/header/Header.js
+++ vaccine_management_system/src/components/header/Header.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import './Header.css';
+import { NavLink } from 'react-router-dom';
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+} from 'reactstrap';
+import Logo from '../../assets/left3.png';
 
-function Header() {
-  return (
-    <div>Header</div>
-  );
+const Header = () => {
+    return (
+        <div className="Navigation">
+            <Navbar style={{
+                backgroundColor: "#f26d3e",
+                height: "85px",
+            }}>
+                <NavbarBrand href="/" className="mr-auto ml-md-5 Brand">
+                    <img src={Logo} alt="Logo" width="80px" />
+                    <NavbarBrand href="/" lassName="mr-auto ml-md-5 Brand" >Vaccine Management System</NavbarBrand>
+                </NavbarBrand>
+               
+                <Nav className="mr-md-5">
+                    <NavItem>
+                        <NavLink exact to="/" className="NavLink">Home</NavLink>
+                    </NavItem>
+                     <NavItem>
+                        <NavLink exact to="/admin" className="NavLink">Admin</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink exact to="/user" className="NavLink">User</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink exact to="/nurse" className="NavLink">Nurse</NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
+        </div>
+    )
 }
 
 export default Header;
