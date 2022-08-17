3f4d16fe0b88566e2f1d946474bbb670c624a5af
Mahfuzur Rahman <mdmahfuzur980@gmail.com>
2022-08-18 00:03:41 +0600

    we are create Vaccines info table also create Contact file

---
+++ vaccine_management_system/src/components/Admin/Vaccines.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const Vaccines = () => {
+   const [users, setUser] = useState([]);
+
+  useEffect(() => {
+    loadUsers();
+  }, []); 
+
+   const loadUsers = async () => {
+   
+
+  };
+
+  const deleteUser = async id => {
+   
+  };
+
+
+  return (<div className="container">
+      <div className="py-">
+        <h1>Vaccines Information</h1>
+        <table class="table border shadow">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Vaccine Name</th>
+              <th scope="col">Company Name</th>
+              <th scope="col">Import Date</th>
+              <th scope="col">Production Date</th>
+              <th scope="col">Expiry Date</th>
+              <th scope="col">Production Countory</th>
+              <th scope="col">Amount of Vaccines Stored</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+             <tr>
+                 <th scope="row">1</th>
+                 <td>Mark</td>
+                 <td>Dook</td>
+                 <td>23-4-22</td>
+                 <td>22-08-23</td>
+                 <td>22-09-34</td>
+                 <td>India</td>
+                 <td>500k</td>
+             </tr>
+          </tbody>
+          <tbody>
+            {users.map((user, index) => (
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>{user.email}</td>
+                <td>{user.email}</td>
+                <td>{user.email}</td>
+                <td>{user.email}</td>
+                <td>
+                  <Link class="btn btn-primary mr-2" to={`/users/${user.id}`}>
+                    View
+                  </Link>
+                  <Link
+                    class="btn btn-outline-primary mr-2"
+                    to={`/users/edit/${user.id}`}
+                  >
+                    Edit
+                  </Link>
+                  <Link
+                    class="btn btn-danger"
+                    onClick={() => deleteUser(user.id)}
+                  >
+                    Delete
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Vaccines
--- vaccine_management_system/src/components/Main.js
+++ vaccine_management_system/src/components/Main.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Contact from './Admin/Contact';
+import Vaccines from './Admin/Vaccines';
 import Header from './header/Header';
 // import Home from './Home/Home';
 // import Admin from '../components/Admin/Admin';
@@ -14,10 +16,14 @@ const Main = props => {
            <Header />
              {/* <div className="container">
                 <Route path="/admin" component={Admin} />
+                <Route path="/vaccines" component={Vaccines} />
                 <Route path="/user" component={User} />
                 <Route path="/nurse" exact component={Nurse} />
                 <Route path="/" exact component={Home} />
             </div> */}
+            <Vaccines/>
+          
+
         </div>
     )
 }
